f920120860c96a72aea0095a5b078dcf7fdab712
allenres <allen.resulidze@gmail.com>
2026-03-19 13:28:37 -0700

    feat(ui): add progress bar for running executions

--- ui/src/components/executions/Gantt.vue
+++ ui/src/components/executions/Gantt.vue
@@ -4,6 +4,11 @@
         :execution="execution!"
     />
     <template v-else-if="execution && executionsStore.flow">
+        <ExecutionProgress
+            v-if="isRunning"
+            :execution="execution!"
+            class="mb-3"
+        />
         <KSFilter
             :configuration="ganttExecutionFilter"
             :tableOptions="{
@@ -153,6 +158,7 @@
     import ExecutionPending from "./ExecutionPending.vue";
     import OnboardingSuccessPopup from "../onboarding/OnboardingSuccessPopup.vue";
     import SaveExecuteAnimation from "../inputs/SaveExecuteAnimation.vue";
+    import ExecutionProgress from "./ExecutionProgress.vue";
     import KSFilter from "../filter/components/KSFilter.vue";
     import {Comparators, type AppliedFilter} from "../filter/utils/filterTypes";
     import {useGanttExecutionFilter} from "../filter/configurations";
@@ -388,6 +394,10 @@
 
     const isExecutionStarted = computed<boolean>(() => {
         return !!execution.value?.state?.current && !["CREATED", "QUEUED"].includes(execution.value.state.current);
+    });
+
+    const isRunning = computed<boolean>(() => {
+        return !!execution.value && State.isRunning(execution.value.state.current);
     });
 
     const hasValidDate = computed<boolean>(() => isFinite(delta()));
